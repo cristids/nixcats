9f783e27de0f3ba6d8ac4aa887cb7662abca5599
CristiDS <cristids@yahoo.com>
2025-05-21 21:22:32 +0300

    add toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,1 +1,20 @@
-{'akinsho/toggleterm.nvim', version = "*", config = true}
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  config = function()
+    require('toggleterm').setup {
+      -- Optional: customize here
+      open_mapping = [[<C-\>]],
+      direction = 'float',
+      float_opts = {
+        border = 'curved',
+      },
+    }
+  end,
+  keys = {
+    { '<leader>th', '<cmd>ToggleTerm direction=horizontal<CR>', desc = 'Terminal (horizontal)' },
+    { '<leader>tv', '<cmd>ToggleTerm direction=vertical<CR>', desc = 'Terminal (vertical)' },
+    { '<leader>tf', '<cmd>ToggleTerm direction=float<CR>', desc = 'Terminal (float)' },
+    { '<leader>tt', '<cmd>ToggleTerm<CR>', desc = 'Toggle Terminal' },
+  },
+}
